1d934a2a7f1a58efa19d32252c53073412c9451d
TommyNHL <research.tommyn.lok@gmail.com>
2024-02-20 12:38:44 +0100

    CNL modeling with stratification

--- WithStratification/1_deployRFmodel/TrainTestSplit.jl
+++ WithStratification/1_deployRFmodel/TrainTestSplit.jl
@@ -16,35 +16,147 @@ using CSV, DataFrames #, PyCall, Conda, LinearAlgebra, Statistics
 inputDB = CSV.read("D:\\0_data\\databaseOfInternal_withNLs.csv", DataFrame)
 sort!(inputDB, [:INCHIKEY, :SMILES, :PRECURSOR_ION, :CNLmasses])
 
-# creating a table with 3 columns
-dfOutput = DataFrame([[],[],[]], ["SMILES", "INCHIKEY", "FREQUENCY"])
+# creating a table with 2 columns
+dfOutput = DataFrame([[],[]], ["INCHIKEY", "FREQUENCY"])
 size(dfOutput)
 
 count = 0
-str = string(inputDB[1, "SMILES"], inputDB[1, "INCHIKEY"])
+str = inputDB[1, "INCHIKEY"]
 for i in 1:size(inputDB, 1)
-    if (string(inputDB[i, "SMILES"], inputDB[i, "INCHIKEY"]) == str)
-        count += 1
-    elseif (i == size(inputDB, 1))
+    if (i == size(inputDB, 1))
         temp = []
-        push!(temp, inputDB[i, "SMILES"])
+        count += 1
         push!(temp, inputDB[i, "INCHIKEY"])
         push!(temp, count)
         push!(dfOutput, temp)
+    elseif (inputDB[i, "INCHIKEY"] == str)
+        count += 1
     else
         temp = []
-        push!(temp, inputDB[i, "SMILES"])
-        push!(temp, inputDB[i, "INCHIKEY"])
+        push!(temp, inputDB[i-1, "INCHIKEY"])
         push!(temp, count)
         push!(dfOutput, temp)
-        str = string(inputDB[i, "SMILES"], inputDB[i, "INCHIKEY"])
+        str = inputDB[i, "INCHIKEY"]
         count = 1
     end
 end
 
-# 29290 x 3
+# 27211 x 2
 dfOutput
 # save
-# output csv is a 693677 x 4 df
+# output csv is a 27211 x 2 df
 savePath = "D:\\0_data\\countingRows4Leverage.csv"
 CSV.write(savePath, dfOutput)
+
+# comparing, 30684 x 793 df
+inputAllFPDB = CSV.read("D:\\0_data\\dataAllFP_withNewPredictedRiWithStratification.csv", DataFrame)
+sort!(inputAllFPDB, [:INCHIKEY, :SMILES])
+
+# creating a table with 2 columns
+dfOutput2 = DataFrame([[],[]], ["INCHIKEY", "FREQUENCY"])
+size(dfOutput2)
+
+count = 0
+str = inputAllFPDB[1, "INCHIKEY"]
+for i in 1:size(inputAllFPDB, 1)
+    if (i == size(inputAllFPDB, 1))
+        temp = []
+        count += 1
+        push!(temp, inputAllFPDB[i, "INCHIKEY"])
+        push!(temp, count)
+        push!(dfOutput2, temp)
+    elseif (inputAllFPDB[i, "INCHIKEY"] == str)
+        count += 1
+    else
+        temp = []
+        push!(temp, inputAllFPDB[i-1, "INCHIKEY"])
+        push!(temp, count)
+        push!(dfOutput2, temp)
+        str = inputAllFPDB[i, "INCHIKEY"]
+        count = 1
+    end
+end
+
+# 28536 x 2
+dfOutput2
+# save
+# output csv is a 28536 x 2 df
+savePath = "D:\\0_data\\countingRowsInFP4Leverage.csv"
+CSV.write(savePath, dfOutput2)
+
+# creating FP df taking frequency into accounut
+# creating a table with 1+FPs+Ri columns
+dfOutputFP = DataFrame([[]], ["INCHIKEY"])
+for col in names(inputAllFPDB)[3:end]
+    dfOutputFP[:, col] = []
+end
+size(dfOutputFP)  # 0 x 792
+
+for ID in 1:size(dfOutput, 1)
+    println(ID)
+    for i = 1:dfOutput[ID, "FREQUENCY"]
+        temp = []
+        push!(temp, dfOutput[ID, "INCHIKEY"])
+        rowNo = findall(inputAllFPDB.INCHIKEY .== inputAllFPDB[i, "INCHIKEY"][end:end])
+        for col in names(inputAllFPDB)[3:end]
+            push!(temp, inputAllFPDB[rowNo, col])
+        end
+        push!(dfOutputFP, temp)
+    end
+end
+
+# 27211 x 1+790+1 df
+dfOutputFP
+# save
+# output csv is a 27211 x 792 df
+savePath = "D:\\0_data\\dataAllFP_withNewPredictedRiWithStratification_Freq.csv"
+CSV.write(savePath, dfOutputFP)
+
+# Train/Test Split by Leverage
+X = deepcopy(dfOutputFP)
+select!(X, Not([:INCHIKEY, :predictRi]))
+size(X)
+Y = deepcopy(inputDB[:, end])
+size(Y)
+
+#= function leverage_dist(X)   # Set x1 and x2 to your FPs variables
+    h = []
+    for i in ProgressBar(1: size(X,1)) #check dimensions
+        x = X[i,:] 
+        hi = x'*pinv(X'*X)*x
+        push!(h,hi)
+    end   
+    return h
+end
+
+h = leverage_dist(Matrix(X))
+
+function strat_split(leverage=h; limits = limits)
+    n = length(leverage)
+    bin = collect(1:n)
+    for i = 1: (length(limits)-1)
+        bin[limits[i].<= leverage].= i
+    end
+    X_train, X_test, y_train, y_test = train_test_split(collect(1:length(leverage)), leverage, test_size = 0.9, random_state = 42, stratify = bin)
+    return  X_train, X_test, y_train, y_test
+end
+
+function create_train_test_split_strat(total_df, y_data, leverage=h; limits = collect(0.0:0.2:1)) # SET START_COL_X_DATA TO 3!!!
+    X_train_ind, X_test_ind, train_lev, test_lev = strat_split(leverage, limits = limits)
+    # Create train test split of total DataFrame and dependent variables using the chosen parameters
+    X_train = total_df[X_train_ind,:]
+    X_test = total_df[X_test_ind,:]
+    y_train = y_data[X_train_ind]
+    y_test = y_data[X_test_ind]
+    # # Select train and test set of independent variables 
+    # X_train = total_train[:, start_col_X_data:end]
+    # X_test = total_test[:, start_col_X_data:end]
+    return  X_train, X_test, y_train, y_test, train_lev, test_lev
+end
+
+create_train_test_split_strat(X, Y, h) =#
+
+# give 3+21567+1 = 21571 columns
+trainSet, valSet = partitionTrainVal(inputDB, 0.7)  # 70% train 30% Val/Test
+size(trainSet)
+size(valSet)
---
+++ WithStratification/2_prepareCNLmodel/Dfs4CNLmodeling.jl
@@ -0,0 +1,102 @@
+using Pkg
+#Pkg.add("BSON")
+#Pkg.add(PackageSpec(url=""))
+#using BSON
+using CSV, DataFrames, PyCall, Conda, LinearAlgebra, Statistics
+#Conda.add("pubchempy")
+#Conda.add("padelpy")
+#Conda.add("joblib")
+#Conda.add("Numpy")
+## import packages ##
+#pcp = pyimport("pubchempy")
+#pd = pyimport("padelpy")
+#jl = pyimport("joblib")
+#np = pyimport("numpy")
+
+# inputing 5000 x (3+21567) df
+# columns: ENTRY, SMILES, INCHIKEY, CNLmasses...
+inputDB = CSV.read("D:\\0_data\\dataframeCNLsRows.csv", DataFrame)
+sort!(inputDB, [:ENTRY])
+
+# imputing 30684 x (2+791) df, columns include 
+        #SMILES, INCHIKEY, 780 APC2D FPs, 10 Pubchem converted FPs, 
+        #and newly added one (FP-derived predicted Ri)
+inputAllFPDB = CSV.read("D:\\0_data\\dataAllFP_withNewPredictedRi.csv", DataFrame)
+sort!(inputAllFPDB, [:INCHIKEY, :SMILES])
+
+# inputing dfs for separation of the cocamides and non-cocamides datasets
+## 5364 x 931 df 
+inputCocamidesTrain = CSV.read("D:\\0_data\\CocamideExt_Fingerprints_train.csv", DataFrame)
+sort!(inputCocamidesTrain, :SMILES)
+
+## 947 x 931 df
+inputCocamidesTest = CSV.read("D:\\0_data\\CocamideExt_Fingerprints_test.csv", DataFrame)
+sort!(inputCocamidesTest, :SMILES)
+
+# df creating
+dfOnlyCocamides = DataFrame([[],[],[]], ["ENTRY", "SMILES", "INCHIKEY"])
+dfOutsideCocamides = DataFrame([[],[],[]], ["ENTRY", "SMILES", "INCHIKEY"])
+for col in names(inputDB)[4:end]
+    dfOnlyCocamides[:, col] = []
+    dfOutsideCocamides[:, col] = []
+end
+dfOnlyCocamides[:, "FPpredictRi"] = []
+dfOutsideCocamides[:, "FPpredictRi"] = []
+size(dfOnlyCocamides)  # 0 x (3+21567+1)
+size(dfOutsideCocamides)  # 0 x (3+21567+1)
+
+function cocamidesOrNot(DB, i)
+    if (DB[i, "SMILES"] in Array(inputCocamidesTrain[:, "SMILES"]) || DB[i, "SMILES"] in Array(inputCocamidesTest[:, "SMILES"]))
+        return true
+    else
+        return false
+    end
+end
+
+function findRowNumber4Ri(DB, i)
+    if (DB[i, "INCHIKEY"] in Array(inputAllFPDB[:, "INCHIKEY"]))
+        return findall(inputAllFPDB.INCHIKEY .== DB[i, "INCHIKEY"])
+    elseif (DB[i, "SMILES"] in Array(inputAllFPDB[:, "SMILES"]))
+        return findall(inputAllFPDB.SMILES .== DB[i, "SMILES"])
+    end
+end
+    
+function dfExtract(i, columnsCNLs)
+    temp = []
+    push!(temp, inputDB[i, "ENTRY"])
+    push!(temp, inputDB[i, "SMILES"])
+    push!(temp, inputDB[i, "INCHIKEY"])
+    for col in columnsCNLs
+        push!(temp, inputDB[i, col])
+    end
+    return temp
+end
+
+# 5000 x 21571
+inputDB
+
+for i in 1:size(inputDB, 1)
+    if (cocamidesOrNot(inputDB, i) == true)
+        tempRow = dfExtract(i, names(inputDB)[4:end])
+        push!(tempRow, inputAllFPDB[findRowNumber4Ri(inputDB, i)[end:end], "predictRi"][1])
+        push!(dfOnlyCocamides, tempRow)
+    elseif (cocamidesOrNot(inputDB, i) == false)
+        tempRow = dfExtract(i, names(inputDB)[4:end])
+        push!(tempRow, inputAllFPDB[findRowNumber4Ri(inputDB, i)[end:end], "predictRi"][1])
+        push!(dfOutsideCocamides, tempRow)
+    end
+end
+
+# 13 x 21571 df
+dfOnlyCocamides
+
+# 4987 x 21571 df
+dfOutsideCocamides
+
+# outputing dfOnlyCocamides with 13 x (3+21567+1)
+savePath = "D:\\0_data\\dataframeCNLsRows_dfOnlyCocamides.csv"
+CSV.write(savePath, dfOnlyCocamides)
+
+# outputing dfOutsideCocamides with 4987 x (3+21567+1)
+savePath = "D:\\0_data\\dataframeCNLsRows_dfOutsideCocamides.csv"
+CSV.write(savePath, dfOutsideCocamides)
