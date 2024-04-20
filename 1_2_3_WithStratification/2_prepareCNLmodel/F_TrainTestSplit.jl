bd5f45a1acb3267742539a0777f807981780e080
TommyNHL <research.tommyn.lok@gmail.com>
2024-04-21 02:03:33 +0800

    123_F_TrainTestSplit.jl

--- 1_2_3_WithStratification/2_prepareCNLmodel/F_TrainTestSplit.jl
+++ 1_2_3_WithStratification/2_prepareCNLmodel/F_TrainTestSplit.jl
@@ -1,54 +1,28 @@
-VERSION
 using Pkg
 #Pkg.add("ScikitLearn")
-#Pkg.add("Plots")
-#Pkg.add("ProgressBars")
-import Conda
-Conda.PYTHONDIR
-ENV["PYTHON"] = raw"C:\Users\user\AppData\Local\Programs\Python\Python311\python.exe"  # python 3.11
-Pkg.build("PyCall")
-Pkg.status()
 #Pkg.add(PackageSpec(url=""))
 using Random
-#using BSON
-using CSV, DataFrames, Conda, LinearAlgebra, Statistics
-using PyCall
-using StatsPlots
-using Plots
-#using ProgressBars
-#using PyPlot
-#Conda.add("pubchempy")
-#Conda.add("padelpy")
-#Conda.add("joblib")
-## import packages ##
-#using PyCall, Conda                 #using python packages
-#pcp = pyimport("pubchempy")
-pd = pyimport("padelpy")            #calculation of FP
-jl = pyimport("joblib")             # used for loading models
-
-using ScikitLearn  #: @sk_import, fit!, predict
-@sk_import ensemble: RandomForestRegressor
-@sk_import ensemble: RandomForestClassifier
+using CSV, DataFrames
 
 # inputing 693685 x 1 df
-CNLsInfo = CSV.read("F:\\databaseOfInternal_withEntryInfoOnly.csv", DataFrame)
+CNLsInfo = CSV.read("F:\\UvA\\databaseOfInternal_withEntryInfoOnly.csv", DataFrame)
 
 # inputing a 693685 x 1 df
-CNLsInfo2 = CSV.read("F:\\databaseOfInternal_withINCHIKEYInfoOnly.csv", DataFrame)
+CNLsInfo2 = CSV.read("F:\\UvA\\databaseOfInternal_withINCHIKEYInfoOnly.csv", DataFrame)
 
 # inputing 693685 x 1+15961 df
-CNLs = CSV.read("F:\\databaseOfInternal_withNLsOnly.csv", DataFrame)
+CNLs = CSV.read("F:\\UvA\\databaseOfInternal_withNLsOnly.csv", DataFrame)
 
 # inputing 693685 x 1 df
-CNLsY = CSV.read("F:\\databaseOfInternal_withYOnly.csv", DataFrame)
+CNLsY = CSV.read("F:\\UvA\\databaseOfInternal_withYOnly.csv", DataFrame)
 
 ### inputing Index for Train/Test Split
-# 485579 x 1
-X_trainIdxDf = CSV.read("F:\\dataframe_dfTrainSetWithStratification_index.csv", DataFrame)
+# 554948 x 1
+X_trainIdxDf = CSV.read("F:\\UvA\\dataframe_dfTrainSetWithStratification_index.csv", DataFrame)
 X_trainIdx = X_trainIdxDf[:, "INDEX"]
 
-# 208106 x 1
-X_testIdxDf = CSV.read("F:\\dataframe_dfTestSetWithStratification_index.csv", DataFrame)
+# 138737 x 1
+X_testIdxDf = CSV.read("F:\\UvA\\dataframe_dfTestSetWithStratification_index.csv", DataFrame)
 X_testIdx = X_testIdxDf[:, "INDEX"]
 
 
@@ -80,12 +54,12 @@ Y_trainFPRi, Y_testFPRi = create_train_test_split_strat(CNLsY, CNLsY, X_trainIdx
 
 dfTrainSetWithStratification = hcat(X_trainInfo, X_trainInfo2, X_trainCNL, Y_trainFPRi)
 dfTrainSetWithStratification
-# output csv is a 693685*0.7 x 1+1+1+15961+1 df = 485579 x 15965
-savePath = "F:\\dataframe_dfTrainSetWithStratification.csv"
+# output csv is a 693685*0.8 x 1+1+1+15961+1 df = 554948 x 15965
+savePath = "F:\\UvA\\dataframe_dfTrainSetWithStratification.csv"
 CSV.write(savePath, dfTrainSetWithStratification)
 
 dfTestSetWithStratification = hcat(X_testInfo, X_testInfo2, X_testCNL, Y_testFPRi)
 dfTestSetWithStratification
-# output csv is a 693685*0.3 x 1+1+1+15961+1 df = 208106 x 15965
-savePath = "F:\\dataframe_dfTestSetWithStratification.csv"
+# output csv is a 693685*0.2 x 1+1+1+15961+1 df = 138737 x 15965
+savePath = "F:\\UvA\\dataframe_dfTestSetWithStratification.csv"
 CSV.write(savePath, dfTestSetWithStratification)
