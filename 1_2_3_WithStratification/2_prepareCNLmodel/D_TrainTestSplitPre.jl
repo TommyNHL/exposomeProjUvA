894adf90cce0dea9ab36c07fb1674214a6f82ae6
TommyNHL <research.tommyn.lok@gmail.com>
2024-04-20 23:44:50 +0800

    123_D_TrainTestSplitPre.jl

--- 1_2_3_WithStratification/2_prepareCNLmodel/D_TrainTestSplitPre.jl
+++ 1_2_3_WithStratification/2_prepareCNLmodel/D_TrainTestSplitPre.jl
@@ -1,29 +1,27 @@
-VERSION
 using Pkg
-using CSV, DataFrames, Conda, LinearAlgebra, Statistics
-
+using CSV, DataFrames
 
 # inputing 693685 x 3+1+15961 df
-# columns: ENTRY, SMILES, INCHIKEY, ISOTOPICMASS, CNLmasses...
-inputCNLs = CSV.read("F:\\dataframeCNLsRows.csv", DataFrame)
+# columns: ENTRY, SMILES, INCHIKEY, MONOISOTOPICMASS, CNLmasses...
+inputCNLs = CSV.read("F:\\UvA\\dataframeCNLsRows.csv", DataFrame)
 sort!(inputCNLs, [:ENTRY])
 
 CNLs = deepcopy(inputCNLs[:, 4:end])
-savePath = "F:\\databaseOfInternal_withNLsOnly.csv"
+savePath = "F:\\UvA\\databaseOfInternal_withNLsOnly.csv"
 CSV.write(savePath, CNLs)
 
 CNLsInfo = deepcopy(inputCNLs[:, 1:1])
-savePath = "F:\\databaseOfInternal_withEntryInfoOnly.csv"
+savePath = "F:\\UvA\\databaseOfInternal_withEntryInfoOnly.csv"
 CSV.write(savePath, CNLsInfo)
 
 
 # ==============================================================================
 # inputing a 693685 x 792 df
-dfOutputFP = CSV.read("F:\\dataAllFP_withNewPredictedRiWithStratification_Freq.csv", DataFrame)
+dfOutputFP = CSV.read("F:\\UvA\\dataAllFP_withNewPredictedRiWithStratification_Freq.csv", DataFrame)
 CNLsInfo2 = deepcopy(dfOutputFP[:, 1:1])
-savePath = "F:\\databaseOfInternal_withINCHIKEYInfoOnly.csv"
+savePath = "F:\\UvA\\databaseOfInternal_withINCHIKEYInfoOnly.csv"
 CSV.write(savePath, CNLsInfo2)
 
 CNLsY = deepcopy(dfOutputFP[:, end:end])
-savePath = "F:\\databaseOfInternal_withYOnly.csv"
+savePath = "F:\\UvA\\databaseOfInternal_withYOnly.csv"
 CSV.write(savePath, CNLsY)
