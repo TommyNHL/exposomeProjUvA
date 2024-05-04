261ed95a696e2ca4891b6f6081d69479ccd467c4
TommyNHL <research.tommyn.lok@gmail.com>
2024-05-04 20:27:36 +0800

    2_F_TrainTestSplit.jl

--- 1_2_3_WithStratification/2_prepareCNLmodel/F_TrainTestSplit.jl
+++ 1_2_3_WithStratification/2_prepareCNLmodel/F_TrainTestSplit.jl
@@ -4,25 +4,32 @@ using Pkg
 using Random
 using CSV, DataFrames
 
-# inputing 693685 x 1 df
+# inputing 693685-4 x 1 df
+X_IdxDf = CSV.read("F:\\UvA\\dataframe_dfWithStratification_95index.csv", DataFrame)
+
+# inputing 693685-4 x 1 df
 CNLsInfo = CSV.read("F:\\UvA\\databaseOfInternal_withEntryInfoOnly.csv", DataFrame)
+CNLsInfo = CNLsInfo[X_IdxDf[:,1], :]
 
-# inputing a 693685 x 1 df
+# inputing a 693685-4 x 1 df
 CNLsInfo2 = CSV.read("F:\\UvA\\databaseOfInternal_withINCHIKEYInfoOnly.csv", DataFrame)
+CNLsInfo2 = CNLsInfo2[X_IdxDf[:,1], :]
 
-# inputing 693685 x 1+15961 df
+# inputing 693685-4 x 1+15961 df
 CNLs = CSV.read("F:\\UvA\\databaseOfInternal_withNLsOnly.csv", DataFrame)
+CNLs = CNLs[X_IdxDf[:,1], :]
 
-# inputing 693685 x 1 df
+# inputing 693685-4 x 1 df
 CNLsY = CSV.read("F:\\UvA\\databaseOfInternal_withYOnly.csv", DataFrame)
+CNLsY = CNLsY[X_IdxDf[:,1], :]
 
 ### inputing Index for Train/Test Split
-# 346842 x 1
-X_trainIdxDf = CSV.read("F:\\UvA\\dataframe_dfTrainSetWithStratification_index.csv", DataFrame)
+# 346842-2 x 1
+X_trainIdxDf = CSV.read("F:\\UvA\\dataframe_dfTrainSetWithStratification_95index.csv", DataFrame)
 X_trainIdx = X_trainIdxDf[:, "INDEX"]
 
-# 346843 x 1
-X_testIdxDf = CSV.read("F:\\UvA\\dataframe_dfTestSetWithStratification_index.csv", DataFrame)
+# 346843-2 x 1
+X_testIdxDf = CSV.read("F:\\UvA\\dataframe_dfTestSetWithStratification_95index.csv", DataFrame)
 X_testIdx = X_testIdxDf[:, "INDEX"]
 
 
@@ -54,12 +61,12 @@ Y_trainFPRi, Y_testFPRi = create_train_test_split_strat(CNLsY, CNLsY, X_trainIdx
 
 dfTrainSetWithStratification = hcat(X_trainInfo, X_trainInfo2, X_trainCNL, Y_trainFPRi)
 dfTrainSetWithStratification
-# output csv is a 693685*0.5 x 1+1+1+15961+1 df = 346842 x 15965
-savePath = "F:\\UvA\\dataframe_dfTrainSetWithStratification.csv"
+# output csv is a 693685*0.5 x 1+1+1+15961+1 df = 346842-2 x 15965
+savePath = "F:\\UvA\\dataframe_95dfTrainSetWithStratification.csv"
 CSV.write(savePath, dfTrainSetWithStratification)
 
 dfTestSetWithStratification = hcat(X_testInfo, X_testInfo2, X_testCNL, Y_testFPRi)
 dfTestSetWithStratification
-# output csv is a 693685*0.5 x 1+1+1+15961+1 df = 346843 x 15965
-savePath = "F:\\UvA\\dataframe_dfTestSetWithStratification.csv"
+# output csv is a 693685*0.5 x 1+1+1+15961+1 df = 346843-2 x 15965
+savePath = "F:\\UvA\\dataframe_95dfTestSetWithStratification.csv"
 CSV.write(savePath, dfTestSetWithStratification)
